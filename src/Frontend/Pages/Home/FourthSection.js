b90e1d9d2a39092cbc2aa40e6eb9a24c282ddf04
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-08-05 17:02:14 +0600

    Updated build with benefit section contents from bilbytt

--- src/Frontend/Pages/Home/FourthSection.js
+++ src/Frontend/Pages/Home/FourthSection.js
@@ -1,17 +1,63 @@
-import { Box, Button, Container, Heading, Text, VStack } from '@chakra-ui/react';
+import { ArrowForwardIcon, CheckCircleIcon } from '@chakra-ui/icons';
+import { Box, Button, Center, Container, Grid, GridItem, Heading, Image, Link, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { Link as reactLink } from 'react-router-dom';
+import logo from './../../../images/logoL.png';
 
 const FourthSection = () => {
     return (
         <Box as='section' py={16} bg="#F2F6FE">
             <Container maxW='container.xl'>
-                <VStack spacing={5} alignItems='flex-start'>
-                    <Heading fontSize={42}>5 tips til deg som drømmer om båt!</Heading>
-                    <Text fontSize={20}>
-                        Båtsesongen nærmer seg og mange drømmer om å investere i egen båt, men hva slags båt er egentlig rett for deg? Og hva skal du se etter? Vi har spurt daglig leder i Ferd Båt, Rune Westad om noen enkle tips.
-                    </Text>
-                    <Button variant='outline' borderColor='brand.500' height='auto' borderRadius={500} py={3} px={8} fontSize={18} color='brand.500' _hover={{ color: 'white', bg: 'brand.500' }}>Se andre lån</Button>
-                </VStack>
+                <Heading fontSize={42} textAlign='center' mb={10}>Hvorfor innbytte gjennom bilbytt?</Heading>
+                <Grid templateColumns={{ lg: 'repeat(3,1fr)' }} gap={6} alignItems="center">
+                    <GridItem textAlign='center' bg='rgba(255, 255, 255, 0.66)' px={6} py={10} borderRadius={20}>
+                        <VStack spacing={5} alignItems='flex-start'>
+                            <Box textAlign='left'>
+                                <Text fontSize={20} fontWeight={600}>Innbytte</Text>
+                                <Text pb={5}>Få mest mulig for bilen din ved innbytte</Text>
+                            </Box>
+
+                            <VStack spacing={4} alignItems='flex-start'>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Flere bilforhandlere konkurrerer om deg</Box>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Konkurransedyktig tilbud</Box>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Innbytte i løpet av 1 - 2 virkedager</Box>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Du har ingen reklamasjonsplikt</Box>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Vi ordner alt for deg</Box>
+                            </VStack>
+                        </VStack>
+                    </GridItem>
+                    <GridItem textAlign='center' bg='white' px={6} py={10} borderRadius={20}>
+                        <VStack spacing={3}>
+                            <Box fontSize={46} position='relative' _after={{ bg: 'blue.100', content: '""', width: '85px', height: '85px', position: 'absolute', left: '10px', top: '10px', zIndex: -1, borderRadius: '200' }} color="blue.800"><Image height='80px' src={logo}></Image></Box>
+                            <Text pb={5} fontSize={18}>Få mest mulig for bilen din ved innbytte
+
+                            </Text>
+                            <VStack spacing={4} alignItems='flex-start' fontSize={17} fontWeight={600}>
+                                <Box><CheckCircleIcon color='brand.500' mr={2} /> Flere bilforhandlere konkurrerer om deg</Box>
+                                <Box><CheckCircleIcon color='brand.500' mr={2} /> Konkurransedyktig tilbud</Box>
+                                <Box><CheckCircleIcon color='brand.500' mr={2} /> Innbytte i løpet av 1 - 2 virkedager</Box>
+                                <Box><CheckCircleIcon color='brand.500' mr={2} /> Du har ingen reklamasjonsplikt</Box>
+                                <Box><CheckCircleIcon color='brand.500' mr={2} /> Vi ordner alt for deg</Box>
+                            </VStack>
+                        </VStack>
+                    </GridItem>
+                    <GridItem textAlign='center' bg='rgba(255, 255, 255, 0.66)' px={6} py={10} borderRadius={20}>
+                        <VStack spacing={5} alignItems='flex-start'>
+                            <Box textAlign='left'>
+                                <Text fontSize={20} fontWeight={600}>Selge selv</Text>
+                                <Text pb={5}>for å bytte til en annen bil</Text>
+                            </Box>
+
+                            <VStack spacing={4} alignItems='flex-start'>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Flere bilforhandlere konkurrerer om deg</Box>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Konkurransedyktig tilbud</Box>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Innbytte i løpet av 1 - 2 virkedager</Box>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Du har ingen reklamasjonsplikt</Box>
+                                <Box><CheckCircleIcon color='brand.200' mr={2} /> Vi ordner alt for deg</Box>
+                            </VStack>
+                        </VStack>
+                    </GridItem>
+                </Grid>
             </Container>
         </Box>
     );
